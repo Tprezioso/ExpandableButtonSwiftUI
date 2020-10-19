4c470c0076cad1bc09a4b9625e6003f26043e4f9
Tprezioso <tommyprezioso@gmail.com>
2020-10-19 09:33:07 -0400

    Initial Commit

---
+++ ExpandableButton/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExpandableButton
+//
+//  Created by Thomas Prezioso on 10/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExpandableButton/ExpandableButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandableButtonApp.swift
+//  ExpandableButton
+//
+//  Created by Thomas Prezioso on 10/19/20.
+//
+
+import SwiftUI
+
+@main
+struct ExpandableButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
