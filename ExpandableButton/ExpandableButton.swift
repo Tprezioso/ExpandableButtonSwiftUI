e67512ba3303f7b467523a8c4501ac7e83b66101
Tprezioso <tommyprezioso@gmail.com>
2020-10-21 18:39:45 -0400

    first commit

---
+++ ExpandableButton/ExpandableButton.swift
@@ -0,0 +1,77 @@
+//
+//  ExpandableButton.swift
+//  ExpandableButton
+//
+//  Created by Thomas Prezioso on 10/20/20.
+//
+
+import SwiftUI
+
+struct ExpandableButton: View {
+    
+    @State var isExpanded = false
+    
+    var body: some View {
+        VStack {
+            if isExpanded {
+                MenuButtons(buttonImage: "camera.fill")
+                MenuButtons(buttonImage: "record.circle.fill")
+                MenuButtons(buttonImage: "photo")
+            }
+            
+            Button(action: {
+                print("Expandable button tapped!!!")
+                isExpanded.toggle()
+                
+            }) {
+                Image(systemName: "plus.circle.fill")
+                    .resizable()
+                    .frame(width: 75, height: 75)
+                    .foregroundColor(.green)
+            }
+            
+            
+        }
+        .animation(.spring())
+    }
+}
+
+struct ExpandableButton_Previews: PreviewProvider {
+    static var previews: some View {
+        ExpandableButton()
+    }
+}
+
+//import SwiftUI
+//
+//struct ExpandableButton: View {
+//
+//    @State var isExpanded = false
+//
+//    var body: some View {
+//        HStack {
+//            Spacer()
+//            VStack {
+//                Spacer()
+//                if isExpanded {
+//                    MenuButtons(buttonImage: "camera.fill")
+//                    MenuButtons(buttonImage: "record.circle.fill")
+//                    MenuButtons(buttonImage: "photo")
+//                }
+//
+//                Button(action: {
+//                    print("Expandable button tapped!!!")
+//                    isExpanded.toggle()
+//
+//                }) {
+//                    Image(systemName: "plus.circle.fill")
+//                        .resizable()
+//                        .frame(width: 75, height: 75)
+//                        .foregroundColor(.green)
+//                }
+//            }
+//            .animation(.spring())
+//        }.padding()
+//
+//    }
+//}
---
+++ ExpandableButton/MenuButtons.swift
@@ -0,0 +1,29 @@
+//
+//  MenuButtons.swift
+//  ExpandableButton
+//
+//  Created by Thomas Prezioso on 10/20/20.
+//
+
+import SwiftUI
+
+struct MenuButtons: View {
+    var buttonImage: String
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .foregroundColor(.green)
+                .frame(width: 50, height: 50)
+            Image(systemName: buttonImage)
+                .imageScale(.large)
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct MenuButtons_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuButtons(buttonImage: "")
+    }
+}
